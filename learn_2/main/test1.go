83b44ee29ecf86b045b93cd3c5851f3c61c3ebac
awen <502244635@qq.com>
2023-09-07 04:26:25 +0800

    数据类型

---
+++ learn_2/main/test1.go
@@ -0,0 +1,7 @@
+package main
+import "fmt"
+func main() {
+	var i int
+	i = 110
+	fmt.Println("i =",i)
+}
---
+++ learn_2/main/test2.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+func main() {
+	var i int
+	i = 10
+	fmt.Println("i = ",i)
+	var num = 10.12
+	fmt.Println("num = ",num)
+	o := 10
+	fmt.Println("0 = ",o)
+}
